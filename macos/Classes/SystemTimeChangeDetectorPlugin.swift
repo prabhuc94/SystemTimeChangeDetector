a3e4db0aaa57b0fc1dc39a8875d1b11d1930c5a1
Prabhu Chandran <prabhuc94@gmail.com>
2024-05-29 06:26:09 +0530

    Mac listener modified[Still it's not detected]

--- macos/Classes/SystemTimeChangeDetectorPlugin.swift
+++ macos/Classes/SystemTimeChangeDetectorPlugin.swift
@@ -2,36 +2,87 @@ import Cocoa
 import FlutterMacOS
 
 public class SystemTimeChangeDetectorPlugin: NSObject, FlutterPlugin {
+  private var channel: FlutterMethodChannel?
+  private var fileDescriptor: CInt = -1
+  private var timeChangeObserver: NSObjectProtocol?
+  private var source: DispatchSourceFileSystemObject?
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "timechangedetector", binaryMessenger: registrar.messenger)
+    let channel = FlutterMethodChannel(name: "systemtimechangedetector", binaryMessenger: registrar.messenger)
     let instance = SystemTimeChangeDetectorPlugin()
+    instance.channel = channel
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-	  case "startListening":
-	   DispatchQueue.global().async {
-                self.setupTimeChangeListener()
-                DispatchQueue.main.async {
-                    result("startListening invoked")
-                }
-            }
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+  if call.method == "startListening" {
+        startListening()
+        result("Listening for time changes")
+      } else if call.method == "getPlatformVersion" {
+        result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
   }
-  
-  private func setupTimeChangeListener() {
-        // Your code to set up a listener for time change events goes here
-        // You can use NotificationCenter to observe system time changes
-        NotificationCenter.default.addObserver(self, selector: #selector(timeChanged), name: NSNotification.Name.NSSystemClockDidChange, object: nil)
+
+  private func startListening() {
+      // Your code to start listening for time changes
+      // Example: Monitor system time changes or time zone changes
+      // Set up observer for system clock changes
+          timeChangeObserver = DistributedNotificationCenter.default.addObserver(
+            forName: NSNotification.Name.NSSystemClockDidChange,
+            object: nil,
+            queue: nil
+          ) { [weak self] notification in
+            self?.systemClockDidChange()
+          }
+    // Set up file monitoring for timezone changes
+    setupFileMonitor()
     }
 
+    private func setupFileMonitor() {
+        fileDescriptor = open("/etc/localtime", O_EVTONLY)
+        if fileDescriptor == -1 {
+          perror("Unable to open /etc/localtime")
+          return
+        }
+
+        source = DispatchSource.makeFileSystemObjectSource(
+          fileDescriptor: fileDescriptor,
+          eventMask: .write,
+          queue: DispatchQueue.global()
+        )
+
+        source?.setEventHandler { [weak self] in
+          self?.timezoneDidChange()
+        }
+
+        source?.setCancelHandler {
+          close(self.fileDescriptor)
+        }
+
+        source?.resume()
+      }
+
+    private func systemClockDidChange() {
+        print("System time has changed!")
+        notifyFlutter(message: "System time has changed")
+      }
+
+    private func timezoneDidChange() {
+        print("Timezone has changed!")
+        notifyFlutter(message: "Timezone has changed")
+      }
+
+    private func notifyFlutter(message: String) {
+        if let channel = channel {
+          channel.invokeMethod("timeChanged", arguments: message)
+        }
+      }
+
     @objc private func timeChanged() {
-        let channel = FlutterMethodChannel(name: "timechangedetector", binaryMessenger: FlutterMethodChannel(binaryMessenger: NSObject()))
-		channel.invokeMethod("timeChanged", arguments: "Time has changed")
+		if let channel = self.channel {
+              channel.invokeMethod("timeChanged", arguments: "System time has changed")
+            }
+            print("Time changed")
     }
 }
